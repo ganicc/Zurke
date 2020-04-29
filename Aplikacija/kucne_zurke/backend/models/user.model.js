2a0e9cc390b22e65ff8f2e7240017217f0ea4a5d
vida98 <arsic.vida@gmail.com>
2020-04-30 01:31:57 +0200

    Aplikacija-kucne_zurke

---
+++ Aplikacija/kucne_zurke/backend/models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    ime: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    prezime: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    korisnickoIme: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    sifra: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    datumRodj: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    pol: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
