e3b570de7bb33a3abf2f3624f19e9e49b00622ab
vida98 <arsic.vida@gmail.com>
2020-07-10 16:27:09 +0200

    korisnici-izmenjeno

--- Aplikacija/app.js
+++ Aplikacija/app.js
@@ -1,39 +1,40 @@
-const express = require('express');
-const expresLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const bodyParser=require("body-parser");
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
-const Zurka = require('./models/Zurka');
+const express = require("express");
+const expresLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+const Zurka = require("./models/Zurka");
 
-
-
-const app = express()
-app.use(express.static(__dirname + '/views'));
+const app = express();
+app.use(express.static(__dirname + "/views"));
 app.use(bodyParser.json());
-app.use('/uploads',express.static('uploads'));
-require('./config/passport')(passport);
+app.use("/uploads", express.static("uploads"));
+require("./config/passport")(passport);
 
 // Mongo
-const db = require('./config/keys').MongoURI;
+const db = require("./config/keys").MongoURI;
 
 // Connect
-mongoose.connect(db, { useNewUrlParser: true,useUnifiedTopology: true  })
-    .then(() => console.log('Povezan na MongoDB...'))
-    .catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Povezan na MongoDB..."))
+  .catch((err) => console.log(err));
 
 //EJS
 app.use(expresLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
-    saveUninitialized: true
-}));
+    saveUninitialized: true,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -41,39 +42,38 @@ app.use(passport.session());
 app.use(flash());
 
 app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_msg')
-    res.locals.error_msg = req.flash('error_msg')
-    res.locals.error = req.flash('error')
-    next()
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+
+  next();
 });
 
 //Routes
-app.use('/', require('./routes/index'))
-app.use('/users', require('./routes/users'))
-app.use('/zurke', require('./routes/zurke'))
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+app.use("/zurke", require("./routes/zurke"));
 
 //RUTE
-app.get('/pregledzurki', function(req,res){
-    Zurka.find().then(zurka=>{
-        res.render('pregledzurki',{
-            zurka:zurka,
-            name: req.user.name
-        })
-    })
-})
-app.get('/_form_changepassword', function(req,res){
-    res.render('_form_changepassword',{
-        name: req.user.name
-    })
-})
-app.get('/_form_changeusername', function(req,res){
-    res.render('_form_changeusername',{
-        name: req.user.name
-        
-    })
-})
-
+app.get("/pregledzurki", function (req, res) {
+  Zurka.find().then((zurka) => {
+    res.render("pregledzurki", {
+      zurka: zurka,
+      name: req.user.name,
+    });
+  });
+});
+app.get("/_form_changepassword", function (req, res) {
+  res.render("_form_changepassword", {
+    name: req.user.name,
+  });
+});
+app.get("/_form_changeusername", function (req, res) {
+  res.render("_form_changeusername", {
+    name: req.user.name,
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log('Povezan na port 3000...'))
+app.listen(PORT, console.log("Povezan na port 3000..."));
--- Aplikacija/routes/users.js
+++ Aplikacija/routes/users.js
@@ -1,295 +1,308 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const User  = require('../models/User');
-const passport = require('passport');
-const Zurka = require('../models/Zurka');
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const passport = require("passport");
+const Zurka = require("../models/Zurka");
 
 //Login page
-router.get('/login', (req,res)=>{ res.render('login')});
-
-//Register page
-router.get('/register', (req,res)=>{ res.render('register')});
-
-router.post('/register', (req, res) => {
-    const { name, email, password, password2 } = req.body;
-    let errors = [];
-
-    if(!name || !email || !password || !password2)
-    {
-        errors.push({ msg: 'Popunite sve podatke.'});
-    }
-
-    if(password !== password2)
-    {
-        errors.push({ msg: 'Sifre se ne poklapaju'});
-    }
-
-    if(password.length < 6)
-    {
-        errors.push({ msg: 'Sifra mora biti duza od 6 karaktera'});
-    }
-
-    if(errors.length > 0)
-    {
-        res.render('register',{
-            errors,
-            name,
-            email,
-            password,
-            password2
-        });
-    }
-    else
-    {
-        User.findOne({ email: email })
-            .then(user => {
-                if(user)
-                {
-                    errors.push({msg: 'Vec postoji korisnik sa tim emailom.'})
-                    res.render('register',{
-                        errors,
-                        name,
-                        email,
-                        password,
-                        password2
-                    });
-                }
-                else
-                {
-                    const newUser = new User({
-                        name,
-                        email,
-                        password
-                    });
-                    //pretvaranje sifre u bcrypt
-
-                    bcrypt.genSalt(10, (err, salt) => bcrypt.hash(newUser.password, salt, (err,hash) => {
-                        if(err)
-                        {
-                            throw err;
-                        }
-                        newUser.password = hash;
-                        newUser.save()
-                            .then(user=>{
-                                req.flash('success_msg', 'Uspesno ste se registrovali.');
-                                res.redirect('/users/login');
-                            })
-                            .catch(err => console.log(err));
-                    }))
-                }
-            })
-    }
-
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.post('/login', (req, res, next) => {
-    passport.authenticate('local', {
-        successRedirect: '/dashboard',
-        failureRedirect: '/users/login',
-        failureFlash: true
-    })(req,res,next)
-})
+//Register page
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 
-router.get('/logout', (req,res) => {
-    req.logout()
-    req.flash('success_msg', 'Uspesno ste se odjavili.')
-    res.redirect('/users/login')
-})
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
 
-router.post('/update',(req,res,next)=>{
-    var id;
-    console.log("Provera korisnika: " + req.body.organizator);
-    if(req.body.name==req.body.organizator){
-        req.flash('error_msg','Ne mozete reagovati na svoj dogadjaj');
-        res.redirect('/dashboard');
-        return;
-    }
-    User.findOne({name:req.body.organizator}).then(user=>{
-        id=user._id;
-        User.findByIdAndUpdate({_id:id},{routerData:{Dogadjaj:true,userReq:req.body.name}},function(err, result) {
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Popunite sve podatke." });
+  }
+
+  if (password !== password2) {
+    errors.push({ msg: "Sifre se ne poklapaju" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: "Sifra mora biti duza od 6 karaktera" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        errors.push({ msg: "Vec postoji korisnik sa tim emailom." });
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2,
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+        //pretvaranje sifre u bcrypt
+
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
             if (err) {
-                res.send(err);
-              } else {
-
-                                console.log("---> Uspesno! <---")
-                                res.redirect('/dashboard')
-                            }
-                        })
-                    })
-              })     
-
-router.post('/changepassword',(req,res,next)=>
-{
-    const { passwordchange, passwordchange2, passwordchange3 } = req.body
-    var id;
-    let errors=[]
-    User.findOne({name:req.body.name}).then(user => 
-    {
-        bcrypt.compare(passwordchange, user.password, (err, isMatch) => 
-        {
-            if(err) 
-            {                
-                req.flash('error_msg','Doslo je do greske prilikom promene lozinke.')
-                res.redirect('/users/profile')
-                return;
+              throw err;
             }
-            if(isMatch)
-            {
-                if(passwordchange2 !== passwordchange3)
-                {
-                    req.flash('error_msg','Unete sifre se ne poklapaju.')
-                    res.redirect('/users/profile')
-                    return;
-                }
-                if(passwordchange2.length < 6)
-                {
-                    req.flash('error_msg','Nova sifra mora biti duza od 6 karaktera.')
-                    res.redirect('/users/profile')
-                    return;
-                }
-                id=user._id;
-                bcrypt.genSalt(10, (err, salt) => 
-                        bcrypt.hash(passwordchange2, salt, (err, hash) =>
-                            User.findByIdAndUpdate({_id:id},{password:hash},function(err, result) {
-                            if (err) 
-                            {
-                                req.flash('error_msg','Doslo je do greske prilikom menjanja lozinke.')
-                                res.redirect('/users/profile')
-                                return;
-                            } 
-                            else 
-                            {
-                                req.flash('success_msg','Uspesno ste promenili lozinku.')
-                                res.redirect('/users/profile')
-                                return;
-                            }
-                        })
-                        ))
-            }
-            else
-            {
-                req.flash('error_msg','Trenutna sifra nije ispravna.')
-                res.redirect('/users/profile')
-                return;
-            }
-        })
-    }) 
-    
-})
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => {
+                req.flash("success_msg", "Uspesno ste se registrovali.");
+                res.redirect("/users/login");
+              })
+              .catch((err) => console.log(err));
+          })
+        );
+      }
+    });
+  }
+});
 
-router.post('/changeusername',(req,res,next)=>
-{
-    const { namechange } = req.body
-    console.log(namechange)
-    var id;
-    let errors=[]
-    User.findOne({name:namechange}).then(user => 
-    {
-        console.log(user);
-        if(!user) 
-        {
-            User.findOne({name:req.body.name}).then(user=>{
-                id=user._id;
-                User.findByIdAndUpdate({_id:id},{name:namechange},function(err, result) {
-                    if (err) 
-                    {
-                        console.log('Greska')
-                        res.redirect('/users/profile')
-                        return;
-                    } 
-                    else 
-                    {
-                        console.log('Uspesno')
-                        res.redirect('/users/profile')
-                        return;
-                    }
-                })
-            })
-        }
-        else{
-            res.redirect('/users/profile')
-            return;
-        }
-    })   
-})
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })(req, res, next);
+});
 
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "Uspesno ste se odjavili.");
+  res.redirect("/users/login");
+});
+router.post("/pozovi", (req, res, next) => {
+  var id;
+  console.log("Proso sam ovde");
+  console.log(req.body);
+  console.log("Provera selektovanog korisnika: " + req.body.id);
+  console.log("Provera ulogovanog korisnika: " + req.body.korisnik);
+  console.log("Provera selektovanog korisnika: " + req.body.name);
+  if (req.body.name == req.body.korisnik) {
+    req.flash("error_msg", "Ne mozete sami sebe pozvati na događaj");
+    res.redirect("/dashboard");
+    return;
+  }
+  User.findOne({ name: req.body.name }).then((user) => {
+    id = user._id;
+    User.findByIdAndUpdate({ _id: id }, function (err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        console.log("---> Uspesno! <---");
+        res.redirect("/dashboard");
+      }
+    });
+  });
+});
 
-router.post('/delete',(req,res)=>{
-    console.log(req.body)
-    User.findByIdAndDelete({"_id":req.body.id},function(err, result) {
+router.post("/update", (req, res, next) => {
+  var id;
+  console.log("Provera korisnika: " + req.body.organizator);
+  if (req.body.name == req.body.organizator) {
+    req.flash("error_msg", "Ne mozete reagovati na svoj dogadjaj");
+    res.redirect("/dashboard");
+    return;
+  }
+  User.findOne({ name: req.body.organizator }).then((user) => {
+    id = user._id;
+    User.findByIdAndUpdate(
+      { _id: id },
+      { routerData: { Dogadjaj: true, userReq: req.body.name } },
+      function (err, result) {
         if (err) {
           res.send(err);
         } else {
-           
-            res.redirect(req.get('referer'));
+          console.log("---> Uspesno! <---");
+          res.redirect("/dashboard");
         }
-    })
-})
+      }
+    );
+  });
+});
 
+router.post("/changepassword", (req, res, next) => {
+  const { passwordchange, passwordchange2, passwordchange3 } = req.body;
+  var id;
+  let errors = [];
+  User.findOne({ name: req.body.name }).then((user) => {
+    bcrypt.compare(passwordchange, user.password, (err, isMatch) => {
+      if (err) {
+        req.flash("error_msg", "Doslo je do greske prilikom promene lozinke.");
+        res.redirect("/users/profile");
+        return;
+      }
+      if (isMatch) {
+        if (passwordchange2 !== passwordchange3) {
+          req.flash("error_msg", "Unete sifre se ne poklapaju.");
+          res.redirect("/users/profile");
+          return;
+        }
+        if (passwordchange2.length < 6) {
+          req.flash("error_msg", "Nova sifra mora biti duza od 6 karaktera.");
+          res.redirect("/users/profile");
+          return;
+        }
+        id = user._id;
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(passwordchange2, salt, (err, hash) =>
+            User.findByIdAndUpdate({ _id: id }, { password: hash }, function (
+              err,
+              result
+            ) {
+              if (err) {
+                req.flash(
+                  "error_msg",
+                  "Doslo je do greske prilikom menjanja lozinke."
+                );
+                res.redirect("/users/profile");
+                return;
+              } else {
+                req.flash("success_msg", "Uspesno ste promenili lozinku.");
+                res.redirect("/users/profile");
+                return;
+              }
+            })
+          )
+        );
+      } else {
+        req.flash("error_msg", "Trenutna sifra nije ispravna.");
+        res.redirect("/users/profile");
+        return;
+      }
+    });
+  });
+});
 
-router.post('/updateReverse',(req,res,next)=>{
-    var id;
-    
-    console.log("UpdateReverse "+req.user.routerData.userReq)
-
-    User.findOne({name:req.user.routerData.userReq}).then(user=>{
-    User.findByIdAndUpdate({_id:user._id},{routerData:{Odgovor:true}},function(err, result){
-        if (err) {
-            res.send(err);
+router.post("/changeusername", (req, res, next) => {
+  const { namechange } = req.body;
+  console.log(namechange);
+  var id;
+  let errors = [];
+  User.findOne({ name: namechange }).then((user) => {
+    console.log(user);
+    if (!user) {
+      User.findOne({ name: req.body.name }).then((user) => {
+        id = user._id;
+        User.findByIdAndUpdate({ _id: id }, { name: namechange }, function (
+          err,
+          result
+        ) {
+          if (err) {
+            console.log("Greska");
+            res.redirect("/users/profile");
+            return;
           } else {
-            Zurka.findOne({organizator:req.body.name}).then(zurka=>{
-            id=zurka._id;
-            console.log(zurka);
-            var novibroj = zurka.brojljudi-1;
-            Zurka.findByIdAndUpdate({_id:id},{brojljudi:novibroj},function(err, result){
-                if(err)
-                {
-                    res.send(err);
-                }
-                else
-                {
-                    console.log("UPDATE JE ODGOVOR")
-                }
-              
-          })
-        })
+            console.log("Uspesno");
+            res.redirect("/users/profile");
+            return;
+          }
+        });
+      });
+    } else {
+      res.redirect("/users/profile");
+      return;
     }
-    })
-    })
- 
-    User.findOne({name:req.body.name}).then(user=>{
+  });
+});
 
-        id=user._id;
-        User.findByIdAndUpdate({_id:id},{routerData:{Dogadjaj:false}},function(err, result) {
-            if (err) {
-              res.send(err);
-            } else {
-                
-               
-                res.redirect('/dashboard')
-            }
-        })
-    })
-})
+router.post("/delete", (req, res) => {
+  console.log(req.body);
+  User.findByIdAndDelete({ _id: req.body.id }, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.redirect(req.get("referer"));
+    }
+  });
+});
 
+router.post("/updateReverse", (req, res, next) => {
+  var id;
 
-router.post('/updateOdgovor',(req,res,next)=>{
-    var id; 
-    User.findOne({name:req.body.name}).then(user=>{
+  console.log("UpdateReverse " + req.user.routerData.userReq);
 
-        id=user._id;
-        User.findByIdAndUpdate({_id:id},{routerData:{Odgovor:false}},function(err, result) {
-            if (err) {
-              res.send(err);
-            } else {
-                
-               
-                res.redirect('/dashboard')
-            }
-        })
-    })
-    
-})
+  User.findOne({ name: req.user.routerData.userReq }).then((user) => {
+    User.findByIdAndUpdate(
+      { _id: user._id },
+      { routerData: { Odgovor: true } },
+      function (err, result) {
+        if (err) {
+          res.send(err);
+        } else {
+          Zurka.findOne({ organizator: req.body.name }).then((zurka) => {
+            id = zurka._id;
+            console.log(zurka);
+            var novibroj = zurka.brojljudi - 1;
+            Zurka.findByIdAndUpdate(
+              { _id: id },
+              { brojljudi: novibroj },
+              function (err, result) {
+                if (err) {
+                  res.send(err);
+                } else {
+                  console.log("UPDATE JE ODGOVOR");
+                }
+              }
+            );
+          });
+        }
+      }
+    );
+  });
+
+  User.findOne({ name: req.body.name }).then((user) => {
+    id = user._id;
+    User.findByIdAndUpdate(
+      { _id: id },
+      { routerData: { Dogadjaj: false } },
+      function (err, result) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.redirect("/dashboard");
+        }
+      }
+    );
+  });
+});
+
+router.post("/updateOdgovor", (req, res, next) => {
+  var id;
+  User.findOne({ name: req.body.name }).then((user) => {
+    id = user._id;
+    User.findByIdAndUpdate(
+      { _id: id },
+      { routerData: { Odgovor: false } },
+      function (err, result) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.redirect("/dashboard");
+        }
+      }
+    );
+  });
+});
 
 module.exports = router;
